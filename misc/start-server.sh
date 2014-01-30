5b66a17e58710a2c4180f2b4ae726596ab1e887d
Tomoki Aonuma <uasi@uasi.jp>
2014-01-30 16:30:11 +0900

    Support vagrant-dns

--- misc/start-server.sh
+++ misc/start-server.sh
@@ -7,10 +7,20 @@ fi
 
 set -x
 
+# Go to the app directory
 cd /app
 
+# Export environment variables
 source .powenv
 export BUNDLE_PATH=$HOME/bundle
+export PORT=8080
 
+# Install gems
 bundle install --jobs=4
-RACK_ENV=${RACK_ENV:-development} bundle exec ruby app.rb -o 0.0.0.0 -p 8080
+
+# Configure port
+sudo iptables -t nat -F PREROUTING
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port $PORT
+
+# Start server
+RACK_ENV=${RACK_ENV:-development} bundle exec ruby app.rb -o 0.0.0.0 -p $PORT
